f590d20a90a72a08225460385754b6c40cbb64f8
ewriq <102306313+ewriq@users.noreply.github.com>
2025-06-22 09:12:04 +0300

    Create Pull.go

---
+++ Pull.go
@@ -0,0 +1,17 @@
+package pouch
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func Pull(image string) error {
+	cmd := exec.Command("docker", "pull", image)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("pull error: %v\n%s", err, string(output))
+	}
+
+	return nil
+}
