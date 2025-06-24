e900f9ad21e7cd9730388287bbaa50e93e882441
ewriq <102306313+ewriq@users.noreply.github.com>
2025-06-24 13:37:17 +0300

    Update DeleteFile.go

--- DeleteFile.go
+++ DeleteFile.go
@@ -10,7 +10,7 @@ func DeleteFile(containerID, filePath string) error {
 	cmd := exec.Command("docker", "exec", containerID, "rm", "-f", filePath)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("dosya silme hatası: %v, çıktı: %s", err, string(out))
+		return fmt.Errorf("file remove err: %v, output: %s", err, string(out))
 	}
 	return nil
 }
